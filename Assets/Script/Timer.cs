472ee99b1072fd90025fe729db84ae500cc7e8a2
PapiChado <76657333+PapiChado@users.noreply.github.com>
2025-04-13 16:21:06 +0200

    Added functioning Timer

--- Assets/Script/Timer.cs
+++ Assets/Script/Timer.cs
@@ -1,18 +1,36 @@
+using System;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
-[SerializeField] float timeLimit = 60f;
-[SerializeField] GameObject timerUI;
 public class Timer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject loserUI;
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] private float remainingTime;
 
-    // Update is called once per frame
     void Update()
     {
+        loserUI.SetActive(false);
+        remainingTime -= Time.deltaTime;
+        int minutes = Mathf.FloorToInt(remainingTime / 60);
+        int seconds = Mathf.FloorToInt(remainingTime % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        if(remainingTime<= 0)
+        {
+            remainingTime = 0;
+            timerText.text = ("00:00");
+            timerEnded();
+        }
         
+
     }
+
+    void timerEnded()
+    {
+        //do your stuff here.
+        loserUI.SetActive(true);
+    }
+
+
 }
